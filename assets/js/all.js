d02c8bb9827caf74fc115946f0ced57a8d64ae22
chiayinin <chiayinin@gmail.com>
2022-07-30 01:24:41 +0800

    Update 2022-07-29T17:24:34.624Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,18 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
+var hamburger = document.querySelector('.hamburger');
+var navMenu = document.querySelector('.navbar__menu');
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+document.querySelectorAll('.navbar__link').forEach(function (e) {
+  return e.addEventListener('click', function () {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  });
+});
+navMenu.addEventListener('click', function () {
+  console.log("123");
 });
 //# sourceMappingURL=all.js.map
